d3137fff6f7038a3ab7d4f4f6f94c055d90f13e9
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-08-01 13:50:27 +0200

    Initial Commit

---
+++ TabBarAnimated/TabBarAnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarAnimatedApp.swift
+//  TabBarAnimated
+//
+//  Created by Cristina on 2023-08-01.
+//
+
+import SwiftUI
+
+@main
+struct TabBarAnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
